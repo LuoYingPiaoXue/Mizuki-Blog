e6c5fc983cd8b7322d294704fc5866ad93ed3445
落桜飘雪 <2714587763@qq.com>
2025-09-07 19:48:48 +0800

    Fix Errors.

--- src/config.ts
+++ src/config.ts
@@ -181,7 +181,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.png", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
+	avatar: "assets/images/avatar.jpg", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
 	name: "落桜飘雪",
 	bio: "一无所有的 是否值得谈梦呢",
 	links: [
